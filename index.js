72bd1f0eb128aeecb33b32ec92525662696ffdc9
arily <lbsaaaaa@gmail.com>
2020-09-02 14:29:46 +0900

    please work

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ console.log(config)
 
 const app = require('sb-qq-bot-framework/lib/Bot')(config.koishi)
 
-const pluginLoader = require('sb-qq-bot-framework/lib/ContextPluginApply')
-const Loaded = pluginLoader(app, config.contextPlugins)
-// console.log(Loaded.webViews)
+try {
+  const pluginLoader = require('sb-qq-bot-framework/lib/ContextPluginApply')
+  const Loaded = pluginLoader(app, config.contextPlugins)
+  // console.log(Loaded.webViews)
 Loaded.webViews.map(v => {
   console.log(v.name, 'installed on', v.path)
   webServer.use(v.path, v.expressApp)
 })
 webServer.listen(3005, () => console.log('Example app listening on port 3005!'))
+} catch (error) {
+  console.log(error)
+}
+
 
 let count = 0
 const maxTries = 3
