016d06380c05c88347239a1eb72d24a63ff21d00
arily <lbsaaaaa@gmail.com>
2020-09-06 17:05:57 +0900

    update

--- Plugins/arily/BlackFarts/views/recipes.jsx
+++ Plugins/arily/BlackFarts/views/recipes.jsx
@@ -2,12 +2,11 @@ const React = require("react");
 const DefaultLayout = require("./layouts/default.jsx");
 const Menu = require("./components/menu.jsx");
 function HelloMessage(props) {
-  const ElementsOfMessage = Object.entries(props.recipes).map(([menu,recipes]) => 
-    new Menu({ menu, recipes }).render()
-  );
   return (
     <DefaultLayout title={props.title}>
-      <div>{ElementsOfMessage}</div>
+      {Object.entries(props.recipes).map(([menu, recipes])=>
+        <Menu menu={menu} recipes={recipes} key={`menu-${menu}`} />
+      )}
     </DefaultLayout>
   );
 }
