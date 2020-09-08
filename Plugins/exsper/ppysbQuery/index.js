0aa1d24ec36e22ea59ce0b54d345dfd95e30be0d
Starlight Exsper <exsper@163.com>
2020-09-08 19:00:25 +0800

    update dependencies rules & eslint code formatted

--- Plugins/exsper/ppysbQuery/index.js
+++ Plugins/exsper/ppysbQuery/index.js
@@ -1,18 +1,17 @@
-const ppysbQuery = require("ppysb-query-ripple");
+const PPYsbQuery = require('ppysb-query-ripple')
 module.exports.name = 'ppysbQuery'
 module.exports.apply = (ctx, options) => {
-    let pbq = new ppysbQuery(options);
-    ctx.middleware(async (meta, next) => {
-        try {
-            let message = meta.message;
-            let userId = meta.userId;
-            let reply = await pbq.apply(userId, message);
-            if (reply) return meta.$send(`[CQ:at,qq=${userId}]` + "\n" + reply);
-            return next();
-        }
-        catch (ex) {
-            console.log(ex);
-            return next();
-        }
-    });
+  const pbq = new PPYsbQuery(options)
+  ctx.middleware(async (meta, next) => {
+    try {
+      const message = meta.message
+      const userId = meta.userId
+      const reply = await pbq.apply(userId, message)
+      if (reply) return meta.$send(`[CQ:at,qq=${userId}]` + '\n' + reply)
+      return next()
+    } catch (ex) {
+      console.log(ex)
+      return next()
+    }
+  })
 }
