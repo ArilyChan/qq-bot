2cd5b6942e0e69e3d7fe1f4c7448c8f0ccb1c582
Starlight Exsper <exsper@163.com>
2020-09-09 20:02:36 +0800

    fix

---
+++ Plugins/exsper/sillyChooser/index.js
@@ -0,0 +1,21 @@
+const SillyChooser = require('sillychooser')
+module.exports.name = 'sillyChooser'
+module.exports.apply = (ctx, options) => {
+  const sc = new SillyChooser(options)
+  ctx.middleware(async (meta, next) => {
+    try {
+      const message = meta.message
+      const userId = meta.userId
+      const reply = sc.apply(meta.selfId, userId, message)
+      if (!reply) return next()
+      const replyMessage = []
+      if (meta.messageType !== 'private') replyMessage.push(`[CQ:reply,id=${meta.messageId}]`)
+      replyMessage.push(reply)
+      // if (reply) return meta.$send(`[CQ:at,qq=${userId}]` + '\n' + reply)
+      meta.$send(replyMessage.join(''))
+    } catch (ex) {
+      console.log(ex)
+      return next()
+    }
+  })
+}
