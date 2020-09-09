a8d27cb5d4a76423e40d7c8bb2c2be1cc4b00d14
arily <lbsaaaaa@gmail.com>
2020-09-09 19:55:10 +0900

    Update index.js

--- Plugins/exsper/sillyChooser/index.js
+++ Plugins/exsper/sillyChooser/index.js
@@ -7,8 +7,13 @@ module.exports.apply = (ctx, options) => {
       const message = meta.message
       const userId = meta.userId
       const reply = sc.apply(meta.selfId, userId, message)
-      if (reply) return meta.$send(`[CQ:at,qq=${userId}]` + '\n' + reply)
-      return next()
+
+      if (!reply) return next()
+      const replyMessage = []
+      if (meta.messageType !== 'private') replyMessage.push(`[CQ:reply,id=${meta.messageId}]`)
+      replyMessage.push(reply)
+      // if (reply) return meta.$send(`[CQ:at,qq=${userId}]` + '\n' + reply)
+      meta.$send(replyMessage.join(''))
     } catch (ex) {
       console.log(ex)
       return next()
