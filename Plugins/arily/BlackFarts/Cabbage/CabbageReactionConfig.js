67aa775ebf5fa9657d4b4ff74c1de40d08c1074b
arily <lbsaaaaa@gmail.com>
2020-09-11 17:06:56 +0900

    update help

--- Plugins/arily/BlackFarts/Cabbage/CabbageReactionConfig.js
+++ Plugins/arily/BlackFarts/Cabbage/CabbageReactionConfig.js
@@ -2,22 +2,26 @@ const StatusMe = require('./StatusMe')
 const config = require('./cabbageReactionUser')
 
 const usage = {
-  anotherelo: '【叹号】anotherelo <osu用户名或id>',
-  'elo.upload': '【叹号】elo.upload <mp id> [比赛名]:格式示例: EloWeeklyCup Season0 1400-1800。',
-  sendMatchResult: '【叹号】elo.result <MatchId>',
+  anotherelo: '❗️anotherelo <osu用户名或id>',
+  'elo.upload': '❗️elo.upload <mp id> [比赛名]:格式示例: EloWeeklyCup Season0 1400-1800。',
+  sendMatchResult: '❗️elo.result <MatchId>',
   // '排': `【叹号】(排, join) <osu用户名或id> `,
   // '找打': `【叹号】(找打, rival, rivals) `,
   // '不打了': `【叹号】(不打了, drop, quit) `,
   // joinTeam: `【叹号】(team.join, jointeam) <osu用户名或id>#<队伍名>`,
   // quitTeam: `【叹号】(team.quit, quitteam) <osu用户名或id>#<队伍名>`,
   // registerTeam: `【叹号】(team.create, createteam) <队伍名>`,
-  dad: '【叹号】(dad, dad_of) <osu用户名或id>',
+  dad: '❗️(dad, dad_of) <osu用户名或id>',
   // findTeam: `【叹号】(team.find, findteam) <队伍名>`,
   // findTeamsByRank: `【叹号】(team.findbyrank, findteamsbyrank, findteambyrank) <分段>`,
-  say: '【叹号】say <...something>',
+  say: '❗️say <...something>',
   吃啥: '❗️吃啥 [菜单] [特殊需求]',
   加个菜: '❗️加个菜 <菜单> <菜名> (换行)[中人的图片]',
-  倒: '倒 <菜单> <菜名>'
+  倒: '❗️倒 <菜单> <菜名>',
+  'menu.marknsfw': (input) => '❗️menu.marknsfw <菜单>',
+  'recipe.marknsfw': (input) => '❗️recipe.marknsfw <菜单> <菜名>',
+  'menu.marksfw': (input) => '❗️menu.marksfw <菜单>',
+  'recipe.marksfw': (input) => '❗️recipe.marksfw <菜单> <菜名>'
 }
 const desc = {
   anotherelo: '查询elo',
@@ -32,7 +36,11 @@ const desc = {
   dad: '找爸爸',
   findTeam: '找队伍',
   findTeamsByRank: '按照分段搜索队伍',
-  say: '代理发言'
+  say: '代理发言',
+  'menu.marknsfw': (input) => '标记菜单为nsfw',
+  'recipe.marknsfw': (input) => '标记料理为nsfw',
+  'menu.marksfw': (input) => '取消菜单的nfsw标记',
+  'recipe.marksfw': (input) => '取消料理的nsfw标记'
 }
 const mute = () => undefined
 const blackfart = ({ meta, app }) => {
