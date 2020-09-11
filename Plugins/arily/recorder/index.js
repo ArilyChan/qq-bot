defa49ce6315dfb32a67e4114926b943aaea4f34
arily <lbsaaaaa@gmail.com>
2020-09-11 15:51:36 +0900

    save to db

--- Plugins/arily/recorder/index.js
+++ Plugins/arily/recorder/index.js
@@ -1,7 +1,8 @@
 const Storage = require('./storage')
 const web = require('./server')
 let webInited = false
-const storage = new Storage()
+let storage
+
 // 在 a.js, b.js 中编写两个不同的插件
 const recorder = require('./recorder')
 const slipper = require('./slipper')
@@ -11,6 +12,7 @@ module.exports.recorder = recorder
 module.exports.slipper = slipper
 module.exports.Storage = Storage
 module.exports.init = (path, options) => {
+  if (!storage) storage = new Storage(options)
   return storage
 }
 module.exports.webView = (options, storage) => {
--- Plugins/arily/recorder/storage.js
+++ Plugins/arily/recorder/storage.js
@@ -1,10 +1,19 @@
+const connection = require('./database')
+
 class storage {
   constructor (options = {}) {
-    this.messages = new Map()
-    this.events = new Map()
-    this.notices = new Map()
-    this.requests = new Map()
-    this.maxEntries = options.maxEntries || 10384
+    this.options = options
+    this.cache = []
+    this.initdb()
+    // this.maxEntries = options.maxEntries || 10384
+  }
+
+  async initdb () {
+    const db = await connection(this.options)
+    this.messages = db.collection('messages')
+    this.events = db.collection('events')
+    this.notices = db.collection('notices')
+    this.requests = db.collection('requests')
   }
 
   copy (meta) {
@@ -12,15 +21,25 @@ class storage {
   }
 
   record (meta) {
-    meta = this.copy(meta)
+    // meta = this.copy(meta)
     switch (meta.postType) {
       case 'message': {
-        this.messages.set(meta.messageId, meta)
-        if (this.messages.size > this.maxEntries) {
-          Array.from(this.messages.keys())
-            .slice(0, 100)
-            .map(key => this.messages.delete(key))
-        }
+        // this.messages.set(meta.messageId, meta)
+        // if (this.messages.size > this.maxEntries) {
+        //   Array.from(this.messages.keys())
+        //     .slice(0, 100)
+        //     .map(key => this.messages.delete(key))
+        // }
+        this.cache.push({
+          meta,
+          timeout: setTimeout(() => {
+            const self = this.cache.findIndex(op => op.meta.messageId === meta.messageId)
+            this.cache.splice(self, 1)
+            this.messages.insertOne(meta)
+            // meta.$send(`inserted ${meta.messageId}`)
+          }, 1000)
+        })
+        // meta.$send(`schedule insert message ${meta.messageId} 1000ms later`)
         break
       }
       case 'notice':
@@ -36,7 +55,21 @@ class storage {
   delete (meta) {
     switch (meta.postType) {
       case 'message': {
-        this.messages.delete(meta.messageId)
+        // meta.$send(`cancelling insertion of ${meta.messageId}`)
+        const index = this.cache.findIndex(op => op.meta.messageId === meta.messageId)
+        if (index !== undefined) {
+          const cache = this.cache[index]
+          clearTimeout(cache.timeout)
+          this.cache.splice(index, 1)
+          // meta.$send(`canceled the schedule of insertion ${meta.messageId}`)
+        } else {
+          this.messages.deleteOne({
+            messageId: meta.messageId,
+            time: meta.time
+          })
+          // meta.$send(`chain took too long, remove from db ${meta.messageId}`)
+        }
+
         break
       }
       case 'notice':
