794fccd7fbf1b4f23fd4f3181b7c04ae581d7d4e
arily <lbsaaaaa@gmail.com>
2020-09-22 21:39:37 +0900

    Update group.blackFarts.recipe.js

--- config/filters/group.blackFarts.recipe.js
+++ config/filters/group.blackFarts.recipe.js
@@ -1,5 +1,5 @@
 const filters = require('sb-qq-bot-framework/lib/filters')
 module.exports = (...groups) => meta => filters.blockGroup(...groups)(meta).then(result => {
   if (result) return true
-  if (!result) return !(['吃啥', '吃什么', '吃什麼'].some(cmd => meta.$parsed.message.includes(cmd)) && meta.$parsed.prefix !== null)
+  if (!result) return !(['吃啥', '吃什么', '吃什麼', '吃点刺激的'].some(cmd => meta.$parsed.message.includes(cmd)) && meta.$parsed.prefix !== null)
 })
