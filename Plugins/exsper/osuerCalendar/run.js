a17148f128ece7a3bddec351266e08bf0dc6d2c5
Starlight Exsper <exsper@163.com>
2020-10-25 07:15:07 +0800

    await meta.$send

--- Plugins/exsper/osuerCalendar/run.js
+++ Plugins/exsper/osuerCalendar/run.js
@@ -3,13 +3,13 @@
 const Activity = require('./Activity')
 const fs = require('fs')
 
-function run (meta, eventPath) {
+async function run (meta, eventPath) {
   try {
     const qqId = meta.userId
-    fs.readFile(eventPath, (err, data) => {
+    fs.readFile(eventPath, async (err, data) => {
       if (err) {
         console.log(err)
-        return meta.$send('一些不好的事情发生了')
+        return await meta.$send('一些不好的事情发生了')
       }
       const events = JSON.parse(data.toString())
       const activity = new Activity(qqId, events)
@@ -25,11 +25,11 @@ function run (meta, eventPath) {
       statList.badList.map((item) => {
         output = output + '忌：' + item.name + '\n\t' + item.bad + '\n'
       })
-      return meta.$send(output)
+      return await meta.$send(output)
     })
   } catch (ex) {
     console.log(ex)
-    return meta.$send('一些不好的事情发生了')
+    return await meta.$send('一些不好的事情发生了')
   }
 }
 
